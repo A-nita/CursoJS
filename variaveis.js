aad10562d260bc5de22a586ae364c890ff66033e
A_nita <nimoura1@gmail.com>
2020-09-07 22:51:48 -0300

    Iniciando o curso

---
+++ variaveis.js
@@ -0,0 +1,11 @@
+var nome = "Anita"; // string
+var idade = 20;
+var peso = 80.3;
+var humano = true;
+var empregados = ["Ana", "Bia", "Ceci"];
+var pessoa = {
+    nome: "Anita",
+    idade: 20,
+    peso: 80.3,
+    humano: true
+};
