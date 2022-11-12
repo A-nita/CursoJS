d6d68edda570a3560401101daf9566ddaf9063fc
A_nita <nimoura1@gmail.com>
2022-11-12 19:49:44 -0300

    fazendo um exercicio

--- js/app.js
+++ js/app.js
@@ -1,9 +1,49 @@
 console.log("Hello World!");
 
-// Variáveis
+// objetos
+const pessoa = {
+    nome: 'John',
+    idade: 30,
+    peso: 88.6,
+    isAdmin: true
+}
+console.log(`${pessoa.nome} tem ${pessoa.idade} anos.`);
 
-// const
-// let
-// var => 
+// arrays
+const animals = [
+    'Lion',
+    'Monkey',
+    'Cat',
+    { mamíferos: 'Cachorro',
+      répteis: 'Cobra',
+      aves: 'Papagaio' 
+    }
+]
+// funções
 
-function calculateDaysBetweenDates(begin, end) {
+//função anônima/expressio
+const soma = function(a, b) {
+    return a + b;
+}
+
+
+//arrow function
+const subtracao = (a, b) => {
+    return a - b;
+}
+
+//arrow function reduzida
+const multiplicacao = (a, b) => a * b;
+
+//callback function
+function sayMyName(name) {
+    console.log('antes de executar a função callback')
+
+    name()
+
+    console.log('depois de executar a callback')
+}
+
+sayMyName( () => {
+    return 'Callback';
+})
